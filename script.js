3fc00032681ade2758ebda2823380a829b4e0c5f
Johnny Matheus <alunotioficial@gmail.com>
2025-08-18 16:18:47 -0300

    Corrigido erro do Github na animação do JS

---
+++ script.js
@@ -0,0 +1,10 @@
+// Botão de saudação
+document.getElementById("btnSaudacao").addEventListener("click", function() {
+  alert("Olá! Seja bem-vindo ao meu portfólio 😃");
+});
+
+// Botão de modo escuro/claro
+document.getElementById("btnDarkMode").addEventListener("click", function() {
+  document.body.classList.toggle("dark-mode");
+});
+
