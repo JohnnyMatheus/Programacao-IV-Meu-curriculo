01fc0227accc365582fc231bb8c134bba5bd7f34
Johnny Matheus <alunotioficial@gmail.com>
2025-08-18 19:30:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Botão de saudação
-document.getElementById("btnSaudacao").addEventListener("click", function() {
+document.getElementById("btnSaudacao").addEventListener("click", function () {
   alert("Olá! Seja bem-vindo ao meu portfólio 😃");
 });
 
 // Botão de modo escuro/claro
-document.getElementById("btnDarkMode").addEventListener("click", function() {
+document.getElementById("btnDarkMode").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
 
+alert("Olá professor Roberson");
